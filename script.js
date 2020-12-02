3144175857dd59c33d0488d7f271416942b612f4
Vincent Delaleu <dev@vincentdelaleu.com>
2020-12-02 20:49:44 +0100

    new API fetch thru array with local fallback

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+let apiQuotes = [];
+
 function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
-
 function removeLoadingSpinner() {
     if (!loader.hidden) {
         quoteContainer.hidden = false;
@@ -18,52 +19,71 @@ function removeLoadingSpinner() {
     }
 }
 
-// Get Quote from API
-async function getQuoteFromAPI() {
-    showLoadingSpinner();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-//  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    const apiUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
-    // TRUMP QUOTES
-    // const apiUrl = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        // console.log(data);
-        // If Author is blank, fill with 'Unknown Author'
-        if (data.author === '') {
-            authorText.innerText = 'Unknown Author';
-        } else {
-            authorText.innerText = data.author;
-        }
-        // Reduce font size for long quotes
-        if (data.quote.length > 77) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote');
-        }
-        quoteText.innerText = data.quote;
-        // Stop the Loader, Show quote
-        removeLoadingSpinner();
-        // TESTING RECURSIVE CODE AS WITH INFINITE LOOP
-        //throw new Error('oops')
-    } catch (error) {
-        //console.log(error);
-        //getQuoteFromAPI();
+// Show new quote
+function newQuote() {
+    showLoadingSpinner();   
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // console.log(quote);
+    
+    // If Author is blank, fill with 'Unknown Author'
+    if (quote.author === '' || !quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
     }
+    // Check Quote length and reduce font size for long quotes
+    if (quote.text.length > 77) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set the Quote, Hide Loader
+    quoteText.innerText = quote.text;
+    removeLoadingSpinner();
 }
 
+// Get local Quotes from json
+function getQuotesLocally() {
+    // Pick a random quote from localQuotes array
+    const localQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    //console.log(localQuote);
+}
+// Get Quotes from API
+async function getQuotesFromAPI() {
+    showLoadingSpinner();
+    //  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl = 'https://type.fit/api/quotes';
+    // const apiUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
+    // TRUMP'S QUOTES
+    // const apiUrl = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+    // PROXY for CORS
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        // console.log(apiQuotes[12]);
+        newQuote();
+        // TESTING RECURSIVE CODE AS WITH INFINITE LOOP
+        // throw new Error('oops')
+    } catch (error) {
+        // Catch Error Here
+        // console.log(error);
+        getQuoteLocally();
+    }
+}
 // Tweet Quote and prepopulate tweet content
 function tweetQuote() {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} – ${author}`;
     window.open(twitterUrl, '_blank');
 }
-
 // Event Listeners
-newQuoteBtn.addEventListener('click', getQuoteFromAPI);
+newQuoteBtn.addEventListener('click', newQuote);
+// newQuoteBtn.addEventListener('click', getQuoteLocally);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
-getQuoteFromAPI();
+getQuotesFromAPI();
+// getQuotesLocally();
